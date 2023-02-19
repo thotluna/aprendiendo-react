9399a7e793ee2a737451f6922ee91469d7fcdd38
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-02-18 21:29:29 -0400

    separate responsibility

--- projects/tic-tac-toe/src/components/Board/index.jsx
+++ projects/tic-tac-toe/src/components/Board/index.jsx
@@ -1,10 +1,8 @@
-import { useEffect, useState } from 'react'
+import { useEffect } from 'react'
 import confetti from 'canvas-confetti'
 
-import { TURNS } from '../../constants'
-import { checkWinnerFrom, checkEndGame } from '../../logic/board.js'
 import { WinnerModal } from '../WinnerModal'
-import { saveGameToStorage, resetGameStorage } from '../../logic/storage/index.js'
+import { resetGameStorage } from '../../logic/storage/index.js'
 import TurnDisplay from '../TurnDisplay'
 import Game from '../Game'
 import Button from '../Button/index.jsx'
@@ -13,26 +11,17 @@ import styles from './Board.module.css'
 import { usePlay } from '../../hooks/usePlay'
 import Footer from '../Footer'
 import { useBoard } from '../../hooks/useBoard'
+import { useTurn } from '../../hooks/useTurn'
 
 export default function Board ({ players }) {
   const { restartPlay } = usePlay()
-  const { board, resetBoard, updateBoard } = useBoard()
-
-  const [turn, setTurn] = useState(() => {
-    const turnFromStorage = window.localStorage.getItem('turn')
-    return turnFromStorage ?? TURNS.X
-  })
-
-  // null es que no hay ganador, false es que hay un empate
-  const [winner, setWinner] = useState(null)
+  const { board, winner, resetBoard, updateBoard, hasBoardFull } = useBoard()
+  const { turn, changeTurn, resetTurn } = useTurn()
 
   const resetGame = () => {
-    // setBoard(Array(9).fill(null))
-    setTurn(TURNS.X)
-    setWinner(null)
     restartPlay()
-
     resetBoard()
+    resetTurn()
     resetGameStorage()
   }
 
@@ -46,31 +35,34 @@ export default function Board ({ players }) {
   }
 
   useEffect(() => {
-    if (!players.includes(turn) && winner === null) {
-      playComputer()
-    }
+    const timeout = setTimeout(() => {
+      if (!players.includes(turn) && winner === null && !hasBoardFull()) {
+        playComputer()
+      }
+    }, 500)
+    return () => clearTimeout(timeout)
   }, [turn])
+
+  useEffect(() => {
+    if (winner) confetti()
+    console.log({ winner })
+  }, [winner])
 
   const move = (index) => {
     if (board[index] || winner) return
-    const newBoard = updateBoard(index, turn)
+    updateBoard(index, turn)
 
-    const newWinner = checkWinnerFrom(newBoard)
-    if (newWinner) {
-      confetti()
-      setWinner(newWinner)
-      return
-    } else if (checkEndGame(newBoard)) {
-      setWinner(false) // empate
-      return
-    }
+    // const newWinner = checkWinnerFrom(newBoard)
+    // if (newWinner) {
+    //   confetti()
+    //   setWinner(newWinner)
+    //   return
+    // } else if (checkEndGame(newBoard)) {
+    //   setWinner(false) // empate
+    //   return
+    // }
 
-    const newTurn = turn === TURNS.X ? TURNS.O : TURNS.X
-    setTurn(newTurn)
-    saveGameToStorage({
-      board: newBoard,
-      turn: newTurn
-    })
+    changeTurn()
   }
   return (
     <main className={styles.board}>
@@ -80,7 +72,7 @@ export default function Board ({ players }) {
       <TurnDisplay turn={turn} />
       <WinnerModal resetGame={resetGame} winner={winner} />
       <Footer>
-        {players}
+        {turn}
       </Footer>
     </main>
   )
--- projects/tic-tac-toe/src/hooks/useBoard.js
+++ projects/tic-tac-toe/src/hooks/useBoard.js
@@ -1,22 +1,49 @@
-import { useState } from 'react'
-import { getBoardStorage, resetBoardStorage } from '../logic/storage'
+import { useState, useEffect } from 'react'
+import { checkWinnerFrom } from '../logic/board'
+import { getBoardStorage, getWinnerStorage, resetBoardStorage, resetWinnerStorage, saveBoardStorage } from '../logic/storage'
 
 export function useBoard () {
   const [board, setBoard] = useState(() => {
     return JSON.parse(getBoardStorage()) ?? Array(9).fill(null)
   })
 
+  const [winner, setWinner] = useState(getWinnerStorage() || null)
+
   const resetBoard = () => {
     resetBoardStorage()
+    resetWinnerStorage()
+    setWinner(null)
     setBoard(Array(9).fill(null))
   }
 
   const updateBoard = (index, value) => {
+    if (hasBoardFull()) { return }
     const newBoard = [...board]
     newBoard[index] = value
     setBoard(newBoard)
+    saveBoardStorage(board)
     return newBoard
   }
 
-  return { board, setBoard, resetBoard, updateBoard }
+  const validateBoard = () => {
+    const newWinner = checkWinnerFrom(board)
+    if (newWinner) setWinner(newWinner)
+    if (hasBoardFull() && newWinner === null) setWinner(false)
+    console.log({ winner, newWinner })
+  }
+
+  const hasBoardFull = () => {
+    return board.every(cell => cell !== null)
+  }
+
+  const hasEmplyBoard = () => {
+    return board.every(cell => cell === null)
+  }
+
+  useEffect(() => {
+    if (hasEmplyBoard()) return
+    validateBoard()
+  }, [board])
+
+  return { board, winner, setBoard, resetBoard, updateBoard, hasBoardFull }
 }
