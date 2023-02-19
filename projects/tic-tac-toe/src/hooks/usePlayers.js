49f4313bb162a7544085e49534ea7b25ee59dfb2
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-02-18 23:03:59 -0400

    Add resetPlayerStorage

--- projects/tic-tac-toe/src/hooks/usePlayers.js
+++ projects/tic-tac-toe/src/hooks/usePlayers.js
@@ -1,13 +1,20 @@
 import { useState } from 'react'
+import { getPlayStorage, resetPlayersStorage, savePlayersStorage } from '../logic/storage'
 
 export function usePlayers () {
-  const [players, setPlayers] = useState([])
+  const [players, setPlayers] = useState(() => getPlayStorage() || [])
 
   const handlerPlayers = (player) => {
     const playersSave = players.includes(player)
       ? players.filter(value => value !== player)
       : [...players, player]
+    savePlayersStorage(playersSave)
     setPlayers(playersSave)
   }
-  return { players, setPlayers, handlerPlayers }
+
+  const resetPlayers = () => {
+    resetPlayersStorage()
+    setPlayers([])
+  }
+  return { players, setPlayers, handlerPlayers, resetPlayers }
 }
