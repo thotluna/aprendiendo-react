e58ffc9d62842671e4d56b7699adb593a3b66e55
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-02-18 23:05:12 -0400

    Add useGame

---
+++ projects/tic-tac-toe/src/hooks/useGame.js
@@ -0,0 +1,50 @@
+import { useEffect } from 'react'
+
+import { usePlay } from './usePlay.js'
+import { useBoard } from './useBoard.js'
+import { useTurn } from './useTurn.js'
+
+import confetti from 'canvas-confetti'
+
+export function useGame ({ players }) {
+  const { restartPlay } = usePlay()
+  const { board, winner, resetBoard, updateBoard, hasBoardFull } = useBoard()
+  const { turn, changeTurn, resetTurn } = useTurn()
+
+  const resetGame = () => {
+    restartPlay()
+    resetBoard()
+    resetTurn()
+  }
+
+  const playComputer = () => {
+    let index
+    do {
+      index = Math.trunc(Math.random() * board.length)
+    } while (board[index] !== null)
+
+    move(index)
+  }
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (!players.includes(turn) && winner === null && !hasBoardFull()) {
+        playComputer()
+      }
+    }, 500)
+    return () => clearTimeout(timeout)
+  }, [turn])
+
+  useEffect(() => {
+    if (winner) confetti()
+    console.log({ winner })
+  }, [winner])
+
+  const move = (index) => {
+    if (board[index] || winner) return
+    updateBoard(index, turn)
+    changeTurn()
+  }
+
+  return { board, turn, winner, resetGame, move }
+}
