7a210db665ad73f05b68c122822022076b5b195f
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-02-18 23:05:40 -0400

    Default named footer component

--- projects/tic-tac-toe/src/components/Footer/index.jsx
+++ projects/tic-tac-toe/src/components/Footer/index.jsx
@@ -1,6 +1,6 @@
 import styles from './Footer.module.css'
 
-export default function Footer ({ children }) {
+export function Footer ({ children }) {
   return (
     <footer className={styles.footer}>
       {children}
