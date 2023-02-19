1cc8cb875ee4c577ec127a19aa71165daca3fc64
Eladio Feijoo <eladio.feijoo@gmal.com>
2023-02-18 21:25:41 -0400

    Set turn and winner storage

--- projects/tic-tac-toe/src/logic/storage/index.js
+++ projects/tic-tac-toe/src/logic/storage/index.js
@@ -25,10 +25,34 @@ export const getBoardStorage = () => {
   return window.localStorage.getItem('board')
 }
 
-export const saveBoardStorage = () => {
+export const saveBoardStorage = (board) => {
   window.localStorage.setItem('board', JSON.stringify(board))
 }
 
 export const resetBoardStorage = () => {
   window.localStorage.removeItem('board')
+}
+
+export const getTurnStorage = () => {
+  return window.localStorage.getItem('turn')
+}
+
+export const saveTurnStorage = (turn) => {
+  window.localStorage.setItem('turn', turn)
+}
+
+export const resetTurnStorage = () => {
+  window.localStorage.removeItem('turn')
+}
+
+export const saveWinnerStorage = (winner) => {
+  window.localStorage.setItem('winner', winner)
+}
+
+export const getWinnerStorage = () => {
+  return window.localStorage.getItem('winner')
+}
+
+export const resetWinnerStorage = () => {
+  window.localStorage.removeItem('winner')
 }
